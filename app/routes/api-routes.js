27513929688a43778c7ce13b681fb9c28ef96eeb
unknown <you@example.com>
2018-01-25 20:02:50 -0800

    authentication Work

--- app/routes/api-routes.js
+++ app/routes/api-routes.js
@@ -1,47 +1,47 @@
-var express = require("express");
-var router = express.Router();
+// var express = require("express");
+// var router = express.Router();
 
-router.get("/landing", function(req, res) {
-    res.send("you are authenticated!");
-});
-module.exports = router;
+// router.get("/landing", function(req, res) {
+//     res.send("you are authenticated!");
+// });
+// module.exports = router;
 
-var Users = require("../models/users.js");
-
-
-module.exports = function(app) {
+// var Users = require("../models");
 
 
-app.post("/api/", function(req, res) {
-     // console.log(req.body);
-     //console.log('success');
-      Users.create({
+// module.exports = function(app) {
+
+
+// app.post("/api/", function(req, res) {
+//      // console.log(req.body);
+//      //console.log('success');
+//       Users.create({
       
-      email : req.body.userEmail, 
-      firstname : req.body.userFname,
-      lastname : req.body.userLname,
-      user_password : req.body.userPassword,
+//       email : req.body.userEmail, 
+//       firstname : req.body.userFname,
+//       lastname : req.body.userLname,
+//       user_password : req.body.userPassword,
       
 
-      });
-      res.json(true);
-    });
-     // res.json(true);
-     // shant
+//       });
+//       res.json(true);
+//     });
+//      // res.json(true);
+//      // shant
 
-     app.get("/api/profile/:email", function(req, res) {
-       //console.log("R " + req.params.email);
-        // if (req.params.email) {
-           Users.findAll({
-             where: {
-               email: req.params.email
-             }
-            }).then(function(results) {
-            //console.log('s' + res.json(results));
-            res.json(results);
-         });
-       // }
-      });
+//      app.get("/api/profile/:email", function(req, res) {
+//        //console.log("R " + req.params.email);
+//         // if (req.params.email) {
+//            Users.findAll({
+//              where: {
+//                email: req.params.email
+//              }
+//             }).then(function(results) {
+//             //console.log('s' + res.json(results));
+//             res.json(results);
+//          });
+//        // }
+//       });
 
 
-}
+// }
--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var router = express.Router();
-var authCtrl = require("../controllers/auth.ctrl");
+var authCtrl = require("../../controllers/auth.ctrl");
 
-router.post("/signup", authCtrl.register);
+router.post("/register", authCtrl.register);
 router.post("/login", authCtrl.login);
 module.exports = router;
